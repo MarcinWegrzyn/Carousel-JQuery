53a8d2936edb9c4a5a98458af0e78aea1715f3fd
Marcin <wegrzyn.marcin@icloud.com>
2017-12-28 20:45:35 +0100

    carousel JQuery

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(function(){
+  
+  var carouselList = $("#carousel ul");
+  var slideInterval = setInterval(changeSlide, 2500);
+
+  $("#carousel").mouseover(function() {
+  clearInterval(slideInterval);
+  });
+
+  $("#carousel").mouseout(function() {
+  slideInterval = setInterval(changeSlide, 2500);
+  });
+
+  function changeSlide(){
+    carouselList.animate({'marginLeft':-1280}, 500, moveFirstSlide);
+
+    function moveFirstSlide(){
+      var  firstItem = carouselList.find("li:first"),
+           lastItem = carouselList.find("li:last");
+      lastItem.after(firstItem);
+      carouselList.css({marginLeft:-640});
+    }
+  }
+
+   function slideBack(){
+
+    carouselList.animate({'marginLeft':0}, 500, moveFirstSlideBack);
+
+    function moveFirstSlideBack(){
+     var  firstItem = carouselList.find("li:first"),
+          lastItem = carouselList.find("li:last");
+      firstItem.before(lastItem);
+      carouselList.css({marginLeft:-640});
+    }
+  } 
+
+  $("#right").click(function(){
+    changeSlide();
+  });
+
+  $("#left").click(function(){
+    slideBack();
+  });
+});
+
